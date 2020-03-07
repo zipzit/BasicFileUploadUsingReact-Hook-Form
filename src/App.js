30373113c707bd81284bd2f0a29164abc4be8cb1
Zip <zipzit@yahoo.com>
2020-03-06 19:29:10 -0800

    Lets try that last thing again...

--- src/App.js
+++ src/App.js
@@ -1,64 +1,31 @@
-import React from "react";
+// CSS from:
+// https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css
+
+// Input for File format from https://mdbootstrap.com/docs/jquery/forms/file-input/
+
+// https://reactjs.org/docs/uncontrolled-components.html
+
+import React, { useState } from "react";
 import "./styles.css";
-import { useForm } from "react-hook-form";
-import axios from "axios";
 
 const App = () => {
-  const { handleSubmit, register } = useForm({
-    mode: "onBlur"
-  });
+  const [displayWords, setDisplayWords] = useState("Choose Multiple Files");
 
-  // const [imageFile, setImageFile] = useState(null);
-
-  // const handleChange = e => {
-  //   e.persist(); // per ParmentierChristophe react-hook-form/issues/274
-  //   // console.log("e: ", e);
-  //   setImageFile(e.target.files[0]); // oops. Nothing is there..
-  //   console.log(imageFile);
-  // };
-
-  const fileInput = React.createRef();
-
-  const onSubmitFn = data => {
-    // event.preventDefault();  // I believe react-hook-form handles this
-    console.log(
-      "onSubmitFn:",
-      data,
-      "  imageFile: ",
-      fileInput.current.files[0].name
-    );
-    const fd = new FormData();
-    for (var key in data) {
-      fd.append(key, data[key]); // formdata doesn't take objects
-    }
-
-    fd.append(
-      "image",
-      fileInput.current.files[0],
-      fileInput.current.files[0].name
-    );
-    axios
-      .post("https://httpbin.org/post", fd, {
-        onUploadProgress: ProgressEvent => {
-          console.log(
-            "Upload Progress: " +
-              Math.round((ProgressEvent.loaded / ProgressEvent.total) * 100) +
-              "%"
-          );
-        }
-      })
-      .then(res => {
-        console.log("response from server: ", res);
-      });
+  const onChangeFn = e => {
+    e.persist;
+    console.log(e.target.value); // just shows the first file selected...
+    console.log(e.target.files.length);
+    setDisplayWords(e.target.files.length + " file(s) selected...");
   };
 
   return (
-    <>
-       <div className="AppClass">
+    <div>
+      <div className="AppClass">
         <h1>Bootstrap Form</h1>
         <h2>Select multiple files - simple styling example</h2>
       </div>
       <p>&nbsp;</p>
+
       <form>
         <div className="input-group col-md-12">
           <div className="input-group-prepend">
@@ -81,7 +48,7 @@ const App = () => {
           </div>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
